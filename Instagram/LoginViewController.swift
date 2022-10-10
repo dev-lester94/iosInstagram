61e15c83f66368e866f232cc9a72f649e72e6937
dev-lester94 <dev.lester94@gmail.com>
2022-10-09 22:04:27 -0500

    requiredstories

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -6,24 +6,67 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+ 
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = userNameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground:username, password:password) {
+          (user: PFUser?, error: Error?) -> Void in
+          if user != nil {
+            // Do stuff after successful login.
+              self.performSegue(withIdentifier: "loginSegue", sender: nil)
+          } else {
+            // The login failed. Check error to see why.
+              let errorString = error!.localizedDescription
+              print(errorString)
+          }
+        }
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = userNameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground {
+            (succeeded: Bool, error: Error?) -> Void in
+            if let error = error {
+                let errorString = error.localizedDescription
+                print(errorString)
+                // Show the errorString somewhere and let the user try again.
+            } else {
+                // Hooray! Let them use the app now.
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }
+        }
+        
+    }
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
